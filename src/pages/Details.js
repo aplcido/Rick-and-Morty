6e160d75a6744c7e67b9fbb39c7425ffecb961f6
Alberto Oliveira <albertooliveira@Air-de-Alberto.home>
2022-01-11 09:59:41 +0000

    Modified details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 //import Episode from "../components/Episode";
 //import Container from "react-bootstrap/Container";
-//import Row from "react-bootstrap/Row";
+import Row from "react-bootstrap/Row";
+import { Card } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 //import { Image } from "react-bootstrap";
 
@@ -28,6 +29,20 @@ function Details() {
     fetchData(url);
   }, [url]);
 
+  const styles = {
+    card: {
+      backgroundColor: "#B7E0F2",
+      borderRadius: 55,
+      padding: "3rem",
+    },
+    cardImage: {
+      height: "100%",
+      objectFit: "cover",
+      borderRadius: 55,
+      maxWidth: "500px",
+    },
+  };
+
   return (
     <>
       {/* <Container
@@ -35,7 +50,7 @@ function Details() {
           border: "solid blue",
         }}
       > */}
-      <div class="card">
+      {/* <div class="card">
         <div class="row no-gutters">
           <div class="col-auto">
             <img src={data.image} alt="" />
@@ -47,15 +62,41 @@ function Details() {
               <Col>Gender: {data.gender}</Col>
               <Col>Species: {data.species}</Col>
               <Col>
-                Location: {data.location?.name} {/*{data.location?.url} */}
+                Location: {data.location?.name} data.location?.url} 
               </Col>
               <Col>
-                Origin: {data.origin?.name} {/*{data.origin?.url}*/}
+                Origin: {data.origin?.name} {data.origin?.url}
               </Col>
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+
+      <Card className="m-5 border-0 shadow" style={styles.card}>
+        <Row>
+          <Col>
+            <Card.Img src={data.image} style={styles.cardImage} />
+          </Col>
+          <Col>
+            <Card.Body>
+              <Card.Title as="h1">
+                <Col>Name: {data.name}</Col>
+              </Card.Title>
+              <Card.Text as="h4" style={styles.cardText}>
+                <Col>Status: {data.status}</Col>
+                <Col>Gender: {data.gender}</Col>
+                <Col>Species: {data.species}</Col>
+                <Col>
+                  Location: {data.location?.name} {/*{data.location?.url} */}
+                </Col>
+                <Col>
+                  Origin: {data.origin?.name} {/*{data.origin?.url}*/}
+                </Col>
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
 
       {/* <Row>
           <Image width="50%" height="150px" src={data.image}></Image>
